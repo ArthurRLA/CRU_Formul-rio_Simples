62b95fea5fd1bf8baf698305ccf380dcc29909ec
ArthurRLA <arthurdemelorezende@gmail.com>
2025-05-27 15:55:02 -0300

    Reajustando o método de envio do email

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,8 +1,10 @@
 class ContactMailer < ApplicationMailer
-  default to: 'funcionarios@empresa.com'
+  default to: 'funcionarios@empresa.com', from: 'nao-responder@empresa.com'
 
   def contact_email(contact)
     @contact = contact
-    mail(subject: "Nova mensagem de \#{@contact.name}")
+    mail(subject: "Novo formulário recebido de #{@contact.name}") do |format|
+      format.html { render 'contact_email' }
+    end
   end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,4 +3,15 @@ class Contact
   attr_accessor :name, :address, :city, :state, :phone, :message
 
   validates :name, :address, :city, :state, :phone, :message, presence: true
+
+  def self.fake
+    new(
+      name: "Arthur Rezende",
+      address: "Rua Exemplo, 123",
+      city: "Goiânia",
+      state: "GO",
+      phone: "999999999999",
+      message: "Que bacana esse email que me foi enviado"
+    )
+  end
 end
